8aa8782b502758d00a3613841a83934fba15ddef
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-07-14 19:35:20 -0500

    Add eslint plugin for jest.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginJest from 'eslint-plugin-jest';
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   eslintConfigPrettier,
+  eslintPluginJest,
 ]);
